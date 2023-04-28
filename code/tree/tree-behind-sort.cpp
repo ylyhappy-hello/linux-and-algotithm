4de718829c3ed987974464cec2ab1f7b5207ddc6
water <2778335106@qq.com>
2023-04-28 17:03:44 +0800

    mipad

--- code/tree/tree-behind-sort.cpp
+++ code/tree/tree-behind-sort.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<string>
 #include<stack>
+#include<queue>
+#include<algorithm>
 using namespace std;
 template<class N, class V>
 class TreeNode{
@@ -13,6 +15,7 @@ class TreeNode{
     void PreOrder();
     void InfixOrder();
     void PostOrder();
+    void LayerOrder();
     TreeNode(TreeNode *LChildNode, TreeNode *RChildNode, N no, V value){
       this->LChildNode = LChildNode;
       this->RChildNode = RChildNode;
@@ -41,6 +44,10 @@ ostream & operator<<( ostream & os,const TreeNode<N, V> *tn)
     return os;
 }
 
+template<class N, class V>
+void TreeNode<N, V>::LayerOrder(){
+
+}
 template<class N, class V>
 void TreeNode<N, V>::PreOrder(){
   // 传进来的必须是root节点
@@ -80,11 +87,14 @@ class BinaryTree{
   public:
     TreeNode<N, V> *root;
     stack<TreeNode<N, V>> BTstack;
+    queue<TreeNode<N, V>> BTqueue;
     void SetCoutType(char Type);
     void PreOrder();
-    void PreOrderSetCoutType();
     void InfixOrder();
     void PostOrder();
+    void LayerOrder();
+    void PreOrderSetCoutType();
+   // void PreOrderSearch(V value);
     BinaryTree(TreeNode<N, V> *root){
       this->root = root;
     }
@@ -129,8 +139,27 @@ void BinaryTree<N, V>::PostOrder(){
   }
 }
 template<class N, class V>
+void BinaryTree<N, V>::LayerOrder(){
+  BTqueue.push(*this->root);
+  cout << this->root;
+  while (!BTqueue.empty()) {
+    TreeNode<N, V> t = BTqueue.front();
+    BTqueue.pop();
+    if (t.LChildNode != NULL){
+      BTqueue.push(*t.LChildNode);
+      cout << t.LChildNode;
+    } 
+    if (t.RChildNode != NULL){
+      BTqueue.push(*t.RChildNode);
+      cout << t.RChildNode;
+    }
+  }
+}
+template<class N, class V>
 void BinaryTree<N, V>::PreOrderSetCoutType(){
   BTstack.push(*this->root);
+  int LH = 0;
+  int RH = 0;
   while (!BTstack.empty()) {
     TreeNode<N, V> t = BTstack.top();
     t.CoutType = 'B';
@@ -138,12 +167,16 @@ void BinaryTree<N, V>::PreOrderSetCoutType(){
     BTstack.pop();
     if (t.RChildNode != NULL){
       BTstack.push(*t.RChildNode);
+      RH++;
     }
     if (t.LChildNode != NULL){
       BTstack.push(*t.LChildNode);
+      LH++;
     } 
   }
+  cout << "tree height is " << max(RH, LH);
 }
+
 int main(){
   // 创建二叉树
   TreeNode<int, string> t1(NULL, NULL, 1, "宋江");
@@ -151,12 +184,16 @@ int main(){
   TreeNode<int, string> t2(NULL, NULL, 2, "吴用");
   TreeNode<int, string> t3(NULL, NULL, 3, "卢俊义");
   TreeNode<int, string> t4(NULL, NULL, 4, "林冲");
+  TreeNode<int, string> t5(NULL, NULL, 5, "hello");
+  TreeNode<int, string> t6(NULL, NULL, 6, "ylyhappy");
   t1.LChildNode = &t2;
   t1.RChildNode = &t3;
   t3.RChildNode = &t4;
+  t3.LChildNode = &t5;
+  t2.LChildNode = &t6;
   // bt.SetCoutType('B');
-  cout << "前序遍历结果" << endl;
-  bt.PreOrderSetCoutType();
+  // cout << "前序遍历结果" << endl;
+  bt.LayerOrder();
   // bt.PreOrder();
   // cout << "中序遍历结果" << endl;
   // bt.InfixOrder();
